36587ee5bfee76a66ee6b98b932401eaeae5da38
sebast-Nino115 <sebastian010315@gmail.com>
2020-10-05 21:32:05 -0500

    proyecto

---
+++ laboratorio3/src/Parqueadero/Transportes.java
@@ -0,0 +1,15 @@
+
+package Parqueadero;
+
+import java.util.Date;
+
+public class Transportes {
+    public String nombre;
+    public String placa;
+    public Date fechaingreso;
+    public Date fechasalida;
+    
+    public Transportes(){
+    
+}
+}
